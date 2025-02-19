df16355f5ba30d91ea47a94b73e6909428075c51
ia74 <68617740+ia74@users.noreply.github.com>
2025-02-18 20:48:47 -0600

    ASD

--- Freedeck/MainWindow.axaml.cs
+++ Freedeck/MainWindow.axaml.cs
@@ -24,8 +24,8 @@ public partial class MainWindow : Window
     private static readonly string Home = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
     public static string InstallPath = Home + "\\Freedeck";
     public static string AppVersion = "1.0.0";
-    public static string LauncherVersion = "1.0.0-rc7";
-    public static string BuildId = "8a76d4fdc843891aad07b6073ea14b4132286d97";
+    public static string LauncherVersion = "1.0.0-rc7-01";
+    public static string BuildId = "3109908d71d6bea7669a80abc192f76f63f22ce9";
     public static bool AutoUpdaterTestMode = false;
     private bool _isUndergoingModification = false;
     public static MainWindow Instance = null!;
